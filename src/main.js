ddad7c21ade16642ead560c083f6ba4087a2eba1
Lukas Pisarcik <lukas.pisarcik@student.tuke.sk>
2022-12-07 10:49:32 +0100

    profile settings

--- src/main.js
+++ src/main.js
@@ -3,10 +3,25 @@ let arrowJoin = document.querySelector('#joinArrow');
 let dialogJoin = document.querySelector('#join');
 let closeJoin = document.querySelector('#joinClose');
 
+let arrowProfile = document.querySelector('#profileArrow');
+let dialogProfile = document.querySelector('#profileSettings');
+let closeProfile = document.querySelector('#profileClose');
+
+
+// join quiz
 arrowJoin.addEventListener('click', ()=>{
     dialogJoin.showModal();
 });
 
 closeJoin.addEventListener('click', ()=>{
     dialogJoin.close();
+})
+
+// profile settings
+arrowProfile.addEventListener('click', ()=>{
+    dialogProfile.showModal();
+});
+
+closeProfile.addEventListener('click', ()=>{
+    dialogProfile.close();
 })
